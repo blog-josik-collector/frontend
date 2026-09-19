5cb3884cd8b15bfcb9ff578dea4d4aba54625ebb
h06 <newlcb0813@naver.com>
2026-09-19 15:28:16 +0900

    🚧 redirect function 수정

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -1,13 +1,25 @@
-export async function onRequest(context) {
-  const { request, params, env } = context;
+export async function onRequest({ request, params, env }) {
+  if (!env.BACKEND_URL) {
+    return Response.json({ error: 'BACKEND_URL is missing' }, { status: 500 });
+  }
 
   const incomingUrl = new URL(request.url);
 
   const path = Array.isArray(params.path) ? params.path.join('/') : (params.path ?? '');
 
-  const targetUrl = new URL(`/${path}${incomingUrl.search}`, env.BACKEND_URL);
+  const backendBase = env.BACKEND_URL.replace(/\/$/, '');
 
-  return fetch(targetUrl.toString(), {
+  const targetUrl = `${backendBase}/${path}${incomingUrl.search}`;
+  console.log('BACKEND_URL =', env.BACKEND_URL);
+  console.log('request =', request.url);
+  console.log('path =', params.path);
+  console.log({
+    backendBase,
+    path,
+    targetUrl,
+  });
+
+  return fetch(targetUrl, {
     method: request.method,
     headers: request.headers,
     body: request.method === 'GET' || request.method === 'HEAD' ? undefined : request.body,
